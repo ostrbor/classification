0afdd71bc2d490b958ea0424fb441224c7aa7505
ostrbor <ostrbor@gmail.com>
2017-09-22 19:24:47 +0300

    Add save and sub methods to group relations

--- group_relations.py
+++ group_relations.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Union
+from models import Prediction
 
 GroupRelation = namedtuple('GroupRelation', ['base_group', 'derivative_group'])
 
@@ -9,8 +10,27 @@ class GroupRelations:
     Data object: list of tuples where first value is base group,
     second value is derivative group.
     """
+
     def __init__(self, data):
         self.data = self.convert(data)
 
-    def convert(self, data: List[Tuple]) -> List[namedtuple]:
+    def convert(self, data) -> List[namedtuple]:
+        # data: List[Tuples] or GroupRelations
         return [GroupRelation(*x) for x in data]
+
+    def save_results(self):
+        for group_relation in self.data:
+            record = Prediction(category=group_relation.base_group,
+                                eshop_category=group_relation.derivative_group,
+                                set_probability=100)
+            record.save()
+
+    def __sub__(self, other):
+        """
+        To find GroupRelations in self that are not present in other
+        """
+        res = []
+        for group_relation in self.data:
+            if group_relation not in other.data:
+                res.append(group_relation)
+        return GroupRelations(res)
--- tests/test_group_relations.py
+++ tests/test_group_relations.py
@@ -3,7 +3,17 @@ from group_relations import GroupRelations, GroupRelation
 
 
 class GroupsTest(TestCase):
+    def setUp(self):
+        self.data = [('a', 'A'), ('b', 'B')]
+
     def test_convert(self):
-        data = [(1, 2), (3, 4)]
-        expected = [GroupRelation(1, 2), GroupRelation(3, 4)]
-        self.assertEqual(expected, GroupRelations(data).data)
+        expected = [GroupRelation('a', 'A'), GroupRelation('b', 'B')]
+        self.assertEqual(expected, GroupRelations(self.data).data)
+
+    def test_sub(self):
+        data_minor = [('a', 'A')]
+        group_rels_major = GroupRelations(self.data)
+        group_rels_minor = GroupRelations(data_minor)
+        expected = GroupRelations([('b', 'B')])
+        res = group_rels_major - group_rels_minor
+        self.assertEqual(expected.data, res.data)
